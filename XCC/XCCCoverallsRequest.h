09b48f6dc92c48157465fab00c1c70d917a7f385
Macmade <macmade@xs-labs.com>
2015-01-19 23:19:57 +0100

    XCCCoverallsRequest

---
+++ XCC/XCCCoverallsRequest.h
@@ -0,0 +1,13 @@
+//
+//  XCCCoverallsRequest.h
+//  xcode-coveralls
+//
+//  Created by Macmade on 19.01.15.
+//  Copyright (c) 2015 XS-Labs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XCCCoverallsRequest : NSObject
+
+@end
---
+++ XCC/XCCCoverallsRequest.m
@@ -0,0 +1,13 @@
+//
+//  XCCCoverallsRequest.m
+//  xcode-coveralls
+//
+//  Created by Macmade on 19.01.15.
+//  Copyright (c) 2015 XS-Labs. All rights reserved.
+//
+
+#import "XCCCoverallsRequest.h"
+
+@implementation XCCCoverallsRequest
+
+@end
