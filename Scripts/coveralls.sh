6c3c8e5f81949ec1b3a397239b0f17afc31f981a
Macmade <macmade@xs-labs.com>
2015-01-20 19:38:39 +0100

    Trying to get coverage reports...

--- Scripts/coveralls.sh
+++ Scripts/coveralls.sh
@@ -2,4 +2,4 @@
 
 source Scripts/xcenv.sh
 declare -r DIR_BUILD="${OBJECT_FILE_DIR_normal}/${CURRENT_ARCH}/"
-xcode-coveralls --verbose --include XS-C++ $(DIR_BUILD)
+xcode-coveralls --verbose --include XCC $(DIR_BUILD)
