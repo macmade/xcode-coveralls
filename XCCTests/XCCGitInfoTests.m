e42022700fee56f05aeaa19fbd59370f51c44069
macmade <macmade@xs-labs.com>
2015-03-01 10:40:33 +0100

    Setup for the test case.

--- XCCTests/XCCGitInfoTests.m
+++ XCCTests/XCCGitInfoTests.m
@@ -22,9 +22,11 @@
  * THE SOFTWARE.
  ******************************************************************************/
 
-#pragma clang diagnostic ignored "-Wselector"
-
 @interface XCCGitInfoTests: XCTestCase
+
+@property( atomic, readwrite, strong ) NSString * tempPath;
+@property( atomic, readwrite, strong ) NSString * reposPath;
+@property( atomic, readwrite, strong ) NSString * invalidReposPath;
 
 @end
 
@@ -32,12 +34,87 @@
 
 - ( void )setUp
 {
+    NSString * bundleReposPath;
+    NSString * template;
+    char     * buf;
+    char     * result;
+    
     [ super setUp ];
+    
+    template = [ NSTemporaryDirectory() stringByAppendingPathComponent: @"xcode-coveralls.XXXXXX" ];
+    buf      = malloc( strlen( template.fileSystemRepresentation ) + 1 );
+    
+    if( buf == NULL )
+    {
+        return;
+    }
+    
+    strlcpy( buf, template.fileSystemRepresentation, strlen( template.fileSystemRepresentation ) );
+    
+    result = mkdtemp( buf );
+    
+    if( result == NULL )
+    {
+        free( buf );
+        
+        return;
+    }
+    
+    bundleReposPath       = [ [ NSBundle bundleForClass: self.class ] pathForResource: @"repos" ofType: @"" ];
+    self.tempPath         = [ [ NSFileManager defaultManager ] stringWithFileSystemRepresentation: result length: strlen( result ) ];
+    self.reposPath        = [ self.tempPath stringByAppendingPathComponent: @"repos" ];
+    self.invalidReposPath = [ self.tempPath stringByAppendingPathComponent: @"invalid" ];
+    
+    [ [ NSFileManager defaultManager ] createDirectoryAtPath: self.invalidReposPath withIntermediateDirectories: YES attributes: nil error: NULL ];
+    [ [ NSFileManager defaultManager ] copyItemAtPath: bundleReposPath toPath: self.reposPath error: NULL ];
+    [ [ NSFileManager defaultManager ] moveItemAtPath: [ self.reposPath stringByAppendingPathComponent: @"git" ]
+                                       toPath:         [ self.reposPath stringByAppendingPathComponent: @".git" ]
+                                       error:          NULL
+    ];
+    
+    free( buf );
 }
 
 - ( void )tearDown
 {
+    [ [ NSFileManager defaultManager ] removeItemAtPath: self.tempPath error: nil ];
+    
     [ super tearDown ];
+}
+
+- ( void )testSetupTempPath
+{
+    BOOL isDir;
+    
+    isDir = NO;
+    
+    XCTAssertNotNil( self.tempPath );
+    XCTAssertTrue( [ [ NSFileManager defaultManager ] fileExistsAtPath: self.tempPath isDirectory: &isDir ] );
+    XCTAssertTrue( isDir );
+}
+
+- ( void )testSetupReposPath
+{
+    BOOL isDir;
+    
+    isDir = NO;
+    
+    XCTAssertNotNil( self.reposPath );
+    XCTAssertTrue( [ [ NSFileManager defaultManager ] fileExistsAtPath: self.reposPath isDirectory: &isDir ] );
+    XCTAssertTrue( isDir );
+    XCTAssertGreaterThan( [ [ [ NSFileManager defaultManager ] contentsOfDirectoryAtPath: self.reposPath error: NULL ] count ], ( NSUInteger )0 );
+}
+
+- ( void )testSetupInvalidReposPath
+{
+    BOOL isDir;
+    
+    isDir = NO;
+    
+    XCTAssertNotNil( self.invalidReposPath );
+    XCTAssertTrue( [ [ NSFileManager defaultManager ] fileExistsAtPath: self.invalidReposPath isDirectory: &isDir ] );
+    XCTAssertTrue( isDir );
+    XCTAssertEqual( [ [ [ NSFileManager defaultManager ] contentsOfDirectoryAtPath: self.invalidReposPath error: NULL ] count ], ( NSUInteger )0 );
 }
 
 @end
